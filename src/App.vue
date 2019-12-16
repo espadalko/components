4e271d24da4b30fe5a2773f1ee0fe84fbf01aaf4
espadalko <espadalko@me.com>
2019-12-16 17:01:51 +0700

    ---

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <MenuFix />
+    <header class="header">
+      <div class="header__title">
+        <h1> MenuFix </h1>
+      </div>
+      <div class="header__menu">
+        <MenuFix />
+      </div>
+    </header>
+  
   </div>
 </template>
 
@@ -20,11 +28,29 @@ export default {
 </script>
 
 <style lang="stylus">
+
+*
+  box-sizing border-box
+  margin 0
+
 #app
+  border solid 1px grey
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  // text-align center
   color #2c3e50
-  // margin-top 60px
+
+.container
+  margin 0 auto
+  padding 15px
+  height 2000px
+  width 700px
+
+.header
+  // height 200px
+  padding 15px
+  background-color grey
+  &__title
+    height 100px
+
 </style>
--- src/components/MenuFix.vue
+++ src/components/MenuFix.vue
@@ -1,6 +1,10 @@
 <template>
-	<div>
-		<div>Component MenuFix</div>
+	<div class="menu">
+		<div class="menu__item">Item1</div>
+		<div class="menu__item">Item2</div>
+		<div class="menu__item">Item3</div>
+		<div class="menu__item">Item4</div>
+		<div class="menu__item">Item5</div>
 	</div>
 </template>
 
@@ -12,5 +16,11 @@
 </script>
 
 <style lang="stylus">
-		
+	.menu
+		display flex
+		justify-content space-around
+		flex-wrap wrap
+		&__item
+			&:hover
+				cursor pointer
 </style>
