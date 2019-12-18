6eecc2236f8071d9a4ee5f3752893979086c87ca
espadalko <espadalko@me.com>
2019-12-18 12:41:59 +0700

    2

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<div class="cards">
+			<div id="card1" class="card card--3">
+				<div class="card__wrapper">
+					<img src="assets/card3.jpg" alt="rrr">
+				</div>
+			</div>
+			<div id="card2" class="card card--2">
+				<div class="card__wrapper"></div>
+			</div>
+			<div id="card3" class="card card--1">
+				<div class="card__wrapper"></div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	components: {
+	}
 }
 </script>
 
 <style lang="stylus">
+borderWidth = 2px
+
+*
+	box-sizing border-box
+html
+	background-color #0E1215
 #app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+	margin 100px auto
+	width 600px
+.cards
+	display flex
+	justify-content center
+	align-items center
+	height 600px
+.card
+	width 167px
+	margin-left -10px
+	border solid #000 
+	border-width borderWidth 
+	background-color #282828
+	&--1
+		z-index 1
+		height 440px
+	&--2
+		z-index 2
+		height 470px
+	&--3
+		z-index 3
+		height 500px
+	&:hover
+		z-index 4
+		cursor pointer
+		transform scale(1.2, 1.2)
+		box-shadow 0 0 10px #979797
+		border 0px
+	&__wrapper
+		// border solid @border-width #979797
+		border solid borderWidth #000
+		width 100%
+		height 100%
 </style>
